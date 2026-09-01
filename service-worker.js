991b44c9895efb401253edee33b418ed22997f9f
mparsonsut <markd.parsonsut@gmail.com>
2026-08-31 18:57:14 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+const CACHE="food-log-v4-meals-servings-water-1";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(Promise.all([
+  self.clients.claim(),
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+])));
+self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
